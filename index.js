162a5046c98b4c68e3a3fbaad033b10ccb8ce200
azim <mdazimmia53@gmail.com>
2024-08-19 11:11:54 +0600

    change

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-1import {getmovieReviewData} from './dataStore.js';
-const movieData=getmovieReviewData()
+import {getmovieReviewData} from './dataStore.js';
+const movieData=getmovieReviewData();
+let sortAcenDesen = false;
  function init(){
-  printtStatis(movieData)
+  printStatis(movieData)
   printMovilist(movieData);
+  sorting(movieData)
 }
 /*total Status print start */
-function printtStatis(data){
+function printStatis(data){
 const flatMoviesReviews=data.flat();
 /* All movie counter length */
-1q1`qconst totalReviews = flatMoviesReviews.length;
+const totalMovies =data.length;
+const totalReviews = flatMoviesReviews.length;
 const totalRating= flatMoviesReviews.reduce((acc,item)=>{
   return acc + item.rating;
 },0);
@@ -23,17 +26,62 @@ addState(tReviewElement,totalReviews);
 function addState(elem,value){
   const span = document.createElement('span');
   span.innerHTML=value;
-1}
+  elem.appendChild(span)
+}
 /*total Status print end*/
 function printMovilist(data){
 const moviListIds=document.querySelector('#moviListId UL');
   const flatMovie=data.flat();
-  const sorted=flatMovie.toSorted((a,b)=>b.on - a.on);
-  addportedprint(moviListIds,sorted)
+  addportedprint(moviListIds,flatMovie)
 }
-
-function addportedprint(moviListIds, sorted){
-  sorted.map((movie)=>{
+function sorting(data){
+//pass data 
+    document.getElementById("sortedButton").addEventListener('click', ()=>sortingClickHandle(data));
+    document.getElementById("sortByRating").addEventListener('click', ()=>sortingClickHandleRating(data));
+    document.getElementById("groupDitails").addEventListener('click', ()=>reviewsGroupDetails(data));
+  }
+  function sortingClickHandle(data){
+  sortAcenDesen = !sortAcenDesen
+  const moviListIds=document.querySelector('#moviListId UL');
+  const flatMoviesReviews =data.flat();
+ const sortedDate= sortAcenDesen ? flatMoviesReviews.toSorted((a,b)=>b.on - a.on):flatMoviesReviews.toSorted((a,b)=>a.on - b.on);
+  removeAllChild(moviListIds);
+    addportedprint(moviListIds,sortedDate);
+  }
+  function sortingClickHandleRating(data){
+  sortAcenDesen = !sortAcenDesen
+  const moviListIds=document.querySelector('#moviListId UL');
+  const flatMoviesReviews =data.flat();
+ const sortedRating= sortAcenDesen ? flatMoviesReviews.toSorted((a,b)=>b.rating - a.rating):flatMoviesReviews.toSorted((a,b)=>a.rating - b.rating);
+  removeAllChild(moviListIds);
+    addportedprint(moviListIds,sortedRating);
+  }
+  function reviewsGroupDetails(data){
+  const flatMovie = data.flat();
+  const moviListIds=document.querySelector('#moviListId UL');
+  removeAllChild(moviListIds);
+  const groupReviews = Object.groupBy(flatMovie, ({title})=>title)
+  const titleKeys = Reflect.ownKeys(groupReviews);
+  titleKeys.forEach((title)=>{
+  const liEl =document.createElement("li");
+    const hEl =document.createElement('h2');
+    liEl.classList.add('card');
+    hEl.innerHTML=title;
+    //all movie array data access 
+    liEl.appendChild(hEl);
+    const reviews= groupReviews[title];
+    reviews.forEach((views)=>{
+const pEl =document.createElement("p");
+const message = `🥷
+🦸‍♀️By: ${views.by}, Comment: ${views.content}, Rating: ${views.rating}`;
+pEl.innerHTML=message;
+liEl.appendChild(pEl);
+    })
+    moviListIds.appendChild(liEl);
+  });
+  }
+function addportedprint(moviListIds, data){
+  data.map((movie)=>{
    const liElem =document.createElement('li');
    const titleElem =document.createElement('p');
 titleElem.innerHTML=`Movie Name : ${movie.title} - Rating: ${movie.rating}`;
@@ -50,6 +98,12 @@ timeElem.innerHTML="Time: " +  new Intl.DateTimeFormat('en-BANGLA').format(movie
 liElem.appendChild(timeElem);
 moviListIds.appendChild(liElem)
   });
+}
+
+function removeAllChild(parent){
+  while(parent.firstChild){
+    parent.removeChild(parent.firstChild);
+  }
 }
 init();
 
